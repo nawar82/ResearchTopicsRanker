62c9859069629a5538daa4643296595273411116
Nawar Dalila <nawar82@gmail.com>
2024-03-04 22:02:01 +0100

    Added docstring to the two functions and updated README.md file

--- research_topics_ranker/fetch_data.py
+++ research_topics_ranker/fetch_data.py
@@ -10,6 +10,14 @@ query = QUERY
 
 
 def search_pubmed(query):
+    """search PuMed database by interacting with esearch
+
+    Args:
+        query (str): A string of the search terms
+
+    Returns:
+        data (list): A list with all PMIDs of search results
+    """
     base_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
     params = {
         "db": "pubmed",
@@ -35,10 +43,17 @@ def search_pubmed(query):
         print("Error occurred while searching")
         return []
 
-
 id_list = search_pubmed(query)
 
 def fetch_abstracts(id_list):
+    """takes a list of pmid and returs their corresponding abstracts
+
+    Args:
+        id_list (list of PMIDs): Could be the output of the search_pubmed(query) function
+
+    Returns:
+        list of dictionaries: [{'PMID' : PMID, 'ABSTRACT' : abstract}]
+    """
     base_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
     ids = ','.join(id_list)
     params = {
