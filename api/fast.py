cf4203522f17afbbc941fcd40db44e1a62510967
Nawar Dalila <nawar82@gmail.com>
2024-03-07 13:35:44 +0100

    Started a simple design for the frontend where we search and get a sentense as a response

--- api/fast.py
+++ api/fast.py
@@ -3,16 +3,6 @@ from research_topics_ranker.fetch_data import search_pubmed
 
 app = FastAPI()
 
-# Allowing all middleware is optional, but good practice for dev purposes
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
-    allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods
-    allow_headers=["*"],  # Allows all headers
-)
-
-
 @app.get("/")
 def root():
     return {
