42d382c76011ebc5c4776a0e284bf4a33803edeb
Jose Lobato <lobato.jl@gmail.com>
2024-03-08 19:12:02 +0100

    added lda_model.py. So far three functions: fit, topic list, graph

---
+++ research_topics_ranker/lda_model.py
@@ -0,0 +1,44 @@
+from sklearn.decomposition import LatentDirichletAllocation
+
+import matplotlib.pyplot as plt
+
+
+def lda_model_fit(n_components, max_iter, vectorized_documents):
+    lda_model = LatentDirichletAllocation(n_components=n_components, max_iter = max_iter)
+
+    # Fit the LDA on the vectorized documents
+    lda_model.fit(vectorized_documents)
+
+    return lda_model
+
+
+def topics_list(model, vectorizer, top_words):
+    topics = []
+    for idx, topic in enumerate(model.components_):
+        # print("Topic %d:" % (idx))
+        topic = [(vectorizer.get_feature_names_out()[i], topic[i]) for i in topic.argsort()[:-top_words - 1:-1]]
+        # print(topic)
+        topics.append(topic)
+    return topics
+
+def graph_topics(topics):
+    # Calculating the number of rows and columns
+    num_sets = len(topics)
+    num_cols = 2  # Number of columns you want
+    num_rows = -(-num_sets // num_cols)  # Ceiling division to ensure enough rows
+
+    # Plotting each data set
+    fig, axs = plt.subplots(num_rows, num_cols, figsize=(15, 5*num_rows))
+    for i, data in enumerate(topics):
+        row = i // num_cols
+        col = i % num_cols
+        labels = [item[0] for item in data]
+        values = [item[1] for item in data]
+        axs[row, col].barh(labels, values, color='skyblue')
+        axs[row, col].set_ylabel('Keywords')
+        axs[row, col].set_title('Topic {}'.format(i))
+        axs[row, col].invert_yaxis()
+
+    plt.xlabel('Score')
+    plt.tight_layout()
+    plt.show()
