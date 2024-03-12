295c119762dbe1d53b8b9dde8e25104f8505a420
Nawar Dalila <nawar82@gmail.com>
2024-03-12 15:24:01 +0100

    tried with the saved model

--- api/fast.py
+++ api/fast.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from research_topics_ranker.fetch_data import search_pubmed
+import pickle
+import pandas as pd
 
 app = FastAPI()
 
@@ -14,3 +16,19 @@ def root():
 def search(query):
     results = search_pubmed(query)
     return f"There is {len(results)} articles published on your query ({query})"
+
+
+
+@app.get("/analysis")
+def analysis(data):
+
+    # Load the fitted pipeline from the pickle file
+    with open('models/lda_model.pkl', 'rb') as file:
+        model = pickle.load(file)
+
+    document_topic_mixture = pd.DataFrame(model.fit_transform(data['Abstract']),
+                                      columns = [f"topic_{i}" for i in range(1, 11)],
+                                      index = data['PMID'])
+
+    return document_topic_mixture
+#data = pd.DataFrame({'PMID': [123456], 'Abstract': ['This is the first abstract']})
