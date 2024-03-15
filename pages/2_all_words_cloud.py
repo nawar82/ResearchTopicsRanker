4d5bb98d89f330ec932d7f9da90321514a58e8cd
Nawar Dalila <nawar82@gmail.com>
2024-03-15 13:01:10 +0100

    nltk.data.path = ['/home/appuser/nltk_data']

--- pages/2_all_words_cloud.py
+++ pages/2_all_words_cloud.py
@@ -9,7 +9,7 @@ from research_topics_ranker.vectorize import *
 from research_topics_ranker.main import *
 from api.fast import AllWordsCloud
 
-nltk.data.path = ['nltk/corpora/']
+nltk.data.path = ['/home/appuser/nltk_data']
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Title for your app
