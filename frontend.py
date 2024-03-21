c06d2dc5e7086360df4c24259cb144f62b4703c1
Nawar Dalila <nawar82@gmail.com>
2024-03-21 09:46:33 +0100

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -12,7 +12,7 @@ st.markdown('''
     **all words cloud** uses tf-idf vectorizer to weigh words after preprocessing
     all abstracts and plot the top 500 words in a word cloud.
 
-    **top usnique words** is using machine learning techniques to model all
+    **top unique words** is using machine learning techniques to model all
     extracted topis from the extracted abstracts and return the top words/phrases
     that have the heighest weights over all topics. We use LDE for topic modelling.
 
